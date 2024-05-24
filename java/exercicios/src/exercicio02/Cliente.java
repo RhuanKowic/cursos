7806556ee235177bcdeb671d73d78fe5eeb6b702
Rhuan Kowic <rhuanksantos2@gmail.com>
2024-05-24 08:19:59 -0300

    Iniciando exercicio 2. Criando classe Cliente.

---
+++ java/exercicios/src/exercicio02/Cliente.java
@@ -0,0 +1,15 @@
+package exercicio02;
+
+public class Cliente {
+  String nome;
+  int anoNasc;
+  String cpf;
+  String telefone;
+
+  void imprimeDados(){
+    System.out.println("Nome: " + nome);
+    System.out.println("Ano de nascimento: " + anoNasc);
+    System.out.println("CPF: " + cpf);
+    System.out.println("Telefone: " + telefone);
+  }
+}
