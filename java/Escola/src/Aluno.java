dd5cdfb3a965812239d4652ed37bb86e01d4275f
Rhuan Kowic <rhuanksantos2@gmail.com>
2024-05-27 09:12:45 -0300

    Criando classe aluno.

---
+++ java/Escola/src/Aluno.java
@@ -0,0 +1,20 @@
+public class Aluno {
+  private String nome;
+  private int age;
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+}
