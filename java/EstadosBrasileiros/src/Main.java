f782666a8cafba566adea2563f77a6d299e0ed55
Rhuan Kowic <rhuanksantos2@gmail.com>
2024-05-29 10:45:21 -0300

    Testando o enum

--- java/EstadosBrasileiros/src/Main.java
+++ java/EstadosBrasileiros/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    for(EstadoBrasileiro e: EstadoBrasileiro.values()){
+      System.out.println(e.getSigla() + " - " + e.getNome());
+    }
+    EstadoBrasileiro eb = EstadoBrasileiro.MINAS_GERAIS;
+    System.out.println("Moro no estado de " + eb);
   }
 }
