38967587b4151c85bcf7b66b3046029e746c5722
Rhuan Kowic <rhuanksantos2@gmail.com>
2024-05-31 09:30:09 -0300

    Metodos uteis da classe String.

---
+++ python/string/string_1.py
@@ -0,0 +1,14 @@
+curso = " Python "
+# Tirando espaços
+print(curso.strip())
+print(curso.lstrip())
+print(curso.rstrip())
+
+# Junções e centralização
+print(curso.center(10, "#"))
+print(".".join(curso))
+
+# Transformando string upper e lower
+print(curso.lower())
+print(curso.upper())
+print(curso.title())
